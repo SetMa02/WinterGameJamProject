4ab1dc4f41cc5bb3f6cf4eb0f9390225e5842b7b
Максим Майнагашев <maxuser99@mail.ru>
2024-12-07 03:38:20 +0700

    testingplayer

--- WinterGameJam/Assets/Scripts/Fire/FireLevel.cs
+++ WinterGameJam/Assets/Scripts/Fire/FireLevel.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class FireLevel : MonoBehaviour
 {
+    public float HeatPerSecond;
+    public float FireSize;
     
+
+    public void FireLevelUp(float newHeatPerSecond, float newFireSize)
+    {
+        HeatPerSecond = newHeatPerSecond;
+        FireSize = newFireSize;
+    }
+
+    public void UpdateSpriteSize(SpriteRenderer spriteRenderer)
+    {
+        spriteRenderer.size = new Vector2(FireSize, FireSize);
+    }
 }
