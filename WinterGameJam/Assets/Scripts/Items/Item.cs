65b79a9fd2ce3d3b3dcaf89f4b9607378da2d090
makarasty <makarasty123@gmail.com>
2024-12-08 18:46:18 +0200

    MORE FIXEs

--- WinterGameJam/Assets/Scripts/Items/Item.cs
+++ WinterGameJam/Assets/Scripts/Items/Item.cs
@@ -9,15 +9,9 @@ public class Item : ScriptableObject
 	public float heatIncreaseAmount;  // Для Log: увеличение currentHeat
 	public bool IsFuel;
 	public ItemType itemType;         // Тип предмета
-	private Sprite sprite = null;
 
 	public Sprite GetSprite()
 	{
-		if (sprite == null)
-		{
-			sprite = sprites[Random.Range(0, sprites.Count)];
-		}
-
-		return sprite;
+		return sprites[0];
 	}
 }
